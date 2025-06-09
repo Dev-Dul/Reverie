5d1633a8d8bc8e26a08f8954bccaf0a4216eb428
Abdulrahim Jamil <jameelangoabdulraheem@gmail.com>
2025-06-09 11:51:35 +0100

    Skeleton blog component built

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,13 +1,17 @@
+import styles from '../styles/blog.module.css';
+
 
 function Blog(){
     return (
-        <div>
-            <div className="header">
+        <div className={styles.wrapper}>
+            <div className={styles.header}>
                 <h2>Explore Reverie</h2>
             </div>
-            <div className="posts">
-                
+            <div className={styles.posts}>
+
             </div>
         </div>
     )
 }
+
+export default Blog;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Home from './components/home.jsx'
+import Blog from './components/blog.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <Blog />
   </StrictMode>,
 )
